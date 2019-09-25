23458b74f8b7d1dd22164f869ad59b3ff984d9d0
achirashah <shah.ac@husky.neu.edu>
2019-09-25 17:52:19 -0400

    adding test cases

--- webapp/test/user.js
+++ webapp/test/user.js
@@ -2,8 +2,106 @@ process.env.NODE_ENV = "test"
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../webapp/routes/app.route');
-const should = chai.should();
+const app = require('../server');
+var request = require('superagent');
 chai.use(chaiHttp);
+var expect = chai.expect;
 
-// https://medium.com/@tariqul.islam.rony/simple-rest-api-builing-with-mysql-and-express-js-and-testing-with-mocha-and-chai-ed0d19f25f79
+//set up the data we need to pass to the login method
+const userCredentials = {
+    email: "arjun@gmail.com",
+    password: "PaSSword@123"
+}
+const wrongPostCredentials = {
+    email: "arjun@gmail.com",
+    password: "PaSSword@123"
+}
+const wrongPostCredentials2 = {
+    firstname: "testinggitproj",
+    lastname: "test",
+    password: "PaSSword@123"
+}
+
+const wrongCreds = {
+    firstname: "testinggitproj",
+    lastname: "test",
+    email: "arjun@gmail.com",
+    password: "PaSSword@123"
+}
+const validCreds = {
+    firstname: "testinggitproj",
+    lastname: "test",
+    password: "PaSSword@123"
+}
+
+var authenticatedUser = request.agent(app);
+
+describe('GET /user', function (done) {
+
+    it("should get all students record", (done) => {
+        chai.request(app)
+            .get('/v1/user/self')
+            .auth('arjun@gmail.com', 'PaSSword@123')
+            .then((res) => {
+                //assertions
+                expect(res).to.have.status(200);
+                expect(res.body.message).to.be.equal("Auth OK");
+                expect(res.body.errors.length).to.be.equal(0);
+            }).catch(err => {
+                console.log(err.message);
+            }).then(done);
+    });
+    it("wrong creds", (done) => {
+        chai.request(app)
+            .put('/v1/user/self')
+            .auth('arjun@gmail.com', 'PaSSword@123')
+            .send(wrongCreds)
+            .then((res) => {
+                //assertions
+                expect(res).to.have.status(400);
+            }).catch(err => {
+                console.log(err.message);
+            }).then(done);
+    });
+    it("valid creds", (done) => {
+        chai.request(app)
+            .put('/v1/user/self')
+            .auth('arjun@gmail.com', 'PaSSword@123')
+            .send(validCreds)
+            .then((res) => {
+                //assertions
+                expect(res).to.have.status(200);
+            }).catch(err => {
+                console.log(err.message);
+            }).then(done);
+    });
+    it("Unvalid creds", (done) => {
+        chai.request(app)
+            .post('/v1/user/')
+            .send(wrongPostCredentials)
+            .then((res) => {
+                //assertions
+                expect(res).to.have.status(400);
+                done();
+            }).catch(err => {
+                console.log(err.message);
+            });
+    });
+    it("Unvalid 2 creds", (done) => {
+        chai.request(app)
+            .post('/v1/user/')
+            .send(wrongPostCredentials2)
+            .then((res) => {
+                //assertions
+                expect(res).to.have.status(400);
+                done();
+
+            }).catch(err => {
+                done(err);
+            });
+    });
+
+
+});
+ 
+
