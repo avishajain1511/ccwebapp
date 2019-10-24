45cac8586457fbabf26fa7d3a69bbc2d981ca2a9
arjunagrawal <agrawal.arj@northeastern.edu>
2019-10-24 15:22:20 -0400

    Added image controller

--- webapp/controllers/app.controller.js
+++ webapp/controllers/app.controller.js
@@ -1,80 +1,77 @@
 var bcrypt = require('bcrypt');
 var validator = require("email-validator");
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = require('../models/app.model');
 var schema = require('./passwordValidator');
 const uuidv1 = require('uuid/v1');
 
-exports.register = function(req,res){
-    
-    console.log("req",req.body);
-    if(req.body.firstname==null || req.body.lastname==null|| (req.body.firstname).trim().length<1 ||(req.body.lastname).trim().length<1 || req.body.password==null || req.body.email==null){
+exports.register = function (req, res) {
 
-        return res.status(400).send({message: 'Bad Request'
-    })};
+    console.log("req", req.body);
+    if (req.body.firstname == null || req.body.lastname == null || (req.body.firstname).trim().length < 1 || (req.body.lastname).trim().length < 1 || req.body.password == null || req.body.email == null) {
 
-    if(!validator.validate(req.body.email)){return res.status(400).send({ message: 'Bad Request, invalid Email'})};
+        return res.status(400).send({
+            message: 'Bad Request, Parameters are not correct'
+        })
+    };
 
-    if(!schema.validate(req.body.password)){return  res.status(400).send({  message: 'Bad Request, invalid Password'})};
+    if (!validator.validate(req.body.email)) { return res.status(400).send({ message: 'Bad Request, invalid Email' }) };
+
+    if (!schema.validate(req.body.password)) { return res.status(400).send({ message: 'Bad Request, invalid Password' }) };
 
     var salt = bcrypt.genSaltSync(10);
     var hash = bcrypt.hashSync(req.body.password, salt);
-    var today = new Date();    
-    var uuid =uuidv1();
-    var users={
-      id:uuid,
-      firstname: req.body.firstname,
-      lastname:req.body.lastname,
-      email:req.body.email,
-      password:hash,
-      created:today,
-      modified:today
+    var today = new Date();
+    var uuid = uuidv1();
+    var users = {
+        id: uuid,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: hash,
+        created: today,
+        modified: today
     }
-    
-    var selectSql ="SELECT count(email) AS Count  FROM users WHERE email = ?";
-    var insert =[users.email ];
-    var result =  mysql.format(selectSql,insert);
+
+    var selectSql = "SELECT count(email) AS Count  FROM users WHERE email = ?";
+    var insert = [users.email];
+    var result = mysql.format(selectSql, insert);
     connection.query(result, function (error, result, fields) {
         var count = result[0].Count;
-       console.log("------------"+count)
-        if(count>=1){
-           return res.status(400).send({message: 'Bad Request, Invalid Input' });
-        }else{
-            connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-                if (error) {
-                  console.log("Bad Request",error);
-                  res.send({
-                    "code":400,
-                    "failed":"Bad Request"
-                  })
-                }else{
-                    var sql ='SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
-                    var insert =[users.email]
-                    var result =  mysql.format(sql,insert);
-    
-                    connection.query(result, function (error, result, fields) {
-                        if (error) {
-                            console.log("Bad Request",error);
-                            res.send({
-                            "code":400,
-                            "failed":"Bad Request"
-                        })
-                    }
-                    else{
-                        res.status(201).send(result[0]);
-                    }
-                });
-                }
-            });
-        }  
-    });
+        console.log("------------" + count)
+        if (count >= 1) {
+            return res.status(400).send({ message: 'Bad Request, Invalid email' });
+        }
+        connection.query('INSERT INTO users SET ?', users, function (error, results, fields) {
+            if (error) {
+                console.log("Bad Request, cannot insert user", error);
+                res.send({
+                    "code": 400,
+                    "failed": "Bad Request"
+                })
+            }
+            var sql = 'SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
+            var insert = [users.email]
+            var result = mysql.format(sql, insert);
 
-  };
-  exports.update = function(req,res){
+            connection.query(result, function (error, result, fields) {
+                if (error) {
+                    console.log("Bad Request", error);
+                    res.send({
+                        "code": 400,
+                        "failed": "Bad Request"
+                    })
+                }
+                res.status(201).send(result[0]);
+            });
+        });
+    });
+};
+exports.update = function (req, res) {
 
     var today = new Date();
     var token = req.headers['authorization'];
-    if (!token) return res.status(400).send({  message: 'Bad Request' });
+    if (!token) return res.status(401).send({ message: 'Unauthorized, please provide authentication' });
 
     var tmp = token.split(' ');
     var buf = new Buffer(tmp[1], 'base64');
@@ -83,82 +80,80 @@ exports.register = function(req,res){
 
     var username = creds[0];
     var password = creds[1];
-    console.log("Update creds"+ username + " "+ password)
+    console.log("Update creds" + username + " " + password)
     var updateSql = "update users set ";
     var parm = "";
-    var insertParam =[];
+    var insertParam = [];
     var keys = Object.keys(req.body);
 
-    if (username==null || password==null) return res.status(400).send({ message: 'Bad Request' });
-    
-      connection.query('SELECT * FROM users WHERE email = ?',username, function (error, results, fields) {
-          if (error) {
-            return res.status(404).send({ message: 'Bad Request' });
+    if (username == null || password == null) return res.status(400).send({ message: 'Bad Request, wrong authoraztion provided' });
 
-          }else{
-            if(results.length >0){
-              if(bcrypt.compareSync(password,results[0].password) ){
-                console.log("-----------Updating----------------------")
-                  for (var i = 0; i < keys.length; i++) {
-                      parm = parm + keys[i]+"= ?, ";
-                      
-                      if(keys[i]=="password"){
-                        if(!schema.validate(req.body.password)){return res.status(400).send({"failed":"Bad Request, invalid Password"})};
-                          var salt = bcrypt.genSaltSync(10);
-                          var hash = bcrypt.hashSync(req.body[keys[i]], salt);
-                          insertParam.push(hash);
-                        }else if(keys[i]=="firstname" || keys[i]=="lastname"){
-                            if((req.body[keys[i]]).trim().length<1){ return res.status(400).send({"failed":"Bad Request, feilds Cannot be empty"})};
+    connection.query('SELECT * FROM users WHERE email = ?', username, function (error, results, fields) {
+        if (error) {
+            return res.status(404).send({ message: 'Not found, email not found for this user' });
+
+        } else {
+            if (results.length > 0) {
+                if (bcrypt.compareSync(password, results[0].password)) {
+                    console.log("-----------Updating----------------------")
+                    for (var i = 0; i < keys.length; i++) {
+                        parm = parm + keys[i] + "= ?, ";
+                        if (keys[i] == "password") {
+                            if (!schema.validate(req.body.password)) { return res.status(400).send({ "failed": "Bad Request, invalid Password" }) };
+                            var salt = bcrypt.genSaltSync(10);
+                            var hash = bcrypt.hashSync(req.body[keys[i]], salt);
+                            insertParam.push(hash);
+                        } else {
+                            if ((req.body[keys[i]]).trim().length < 1) { return res.status(400).send({ "failed": "Bad Request, feilds Cannot be empty" }) };
                             insertParam.push(req.body[keys[i]]);
                         }
-                  }
-                  if (insertParam.length==0) return res.status(400).send({ message: 'Bad Request' });
+                    }
+                    if (insertParam.length == 0) return res.status(400).send({ message: 'Bad Request, No parameter found to update' });
 
-                  insertParam.push(today);
-                  insertParam.push(username)
-                  var updateSqlQuery = updateSql+ parm + "modified =? WHERE email = ?"
-                  var updateresult =mysql.format(updateSqlQuery,insertParam);
-                      connection.query(updateresult, function (error, result, fields) {
-                          if (error) {
-                              return res.status(400).send({ message: 'Bad Request' });
-                          }else{
-                            var sql ='SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
-                            var insert =[username]
-                            var result =  mysql.format(sql,insert);
-            
+                    insertParam.push(today);
+                    insertParam.push(username)
+                    var updateSqlQuery = updateSql + parm + "modified =? WHERE email = ?"
+                    var updateresult = mysql.format(updateSqlQuery, insertParam);
+                    connection.query(updateresult, function (error, result, fields) {
+                        if (error) {
+                            return res.status(400).send({ message: 'Bad Request' });
+                        } else {
+                            var sql = 'SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
+                            var insert = [username]
+                            var result = mysql.format(sql, insert);
+
                             connection.query(result, function (error, result, fields) {
                                 if (error) {
-                                    console.log("Bad Request",error);
-                                    res.send({
-                                    "code":400,
-                                    "failed":"Bad Request"
-                                })
-                            }
-                            else{
-                                res.status(204).send({  message: 'No Content' });
-                            }
-                        });
-                          }
-                      });
-               
-              }else{
-                return res.status(401).send({ message: 'Unauthorized' });
-          } 
-            }
-            else{
-                return res.status(404).send({ message: 'Content Not Found' });
-            }
-              
-            }
-        });  
-  }
+                                    console.log("Bad Request", error);
+                                    res.status(401).send({
+                                       message: "Not found, user not found"
+                                    })
+                                }
+                                else {
+                                    res.status(204).send({ message: 'No Content' });
+                                }
+                            });
+                        }
+                    });
 
-  exports.login = function(req,res){
-      
+                } else {
+                    return res.status(401).send({ message: 'Unauthorized, password do not match to current user' });
+                }
+            }
+            else {
+                return res.status(404).send({ message: 'User Not Found' });
+            }
+
+        }
+    });
+}
+
+exports.login = function (req, res) {
+
     var token = req.headers['authorization'];
 
     if (!token) return res.status(401).send({ message: 'Unauthorization' });
-    
+
     var tmp = token.split(' ');
     var buf = new Buffer(tmp[1], 'base64');
     var plain_auth = buf.toString();
@@ -166,39 +161,39 @@ exports.register = function(req,res){
 
     var username = creds[0];
     var password = creds[1];
-    
-    
-    if (username==null || password==null) {
+
+
+    if (username == null || password == null) {
         return res.status(400).send({ message: 'Bad Request' });
-        }
-        console.log("user" +  username, "password " + password );
-        connection.query('SELECT * FROM users WHERE email = ?',username, function (error, results, fields) {
-            if (error) {
-                return res.status(404).send({ message: 'Bad Request' });
-            }else{
-                if(results.length >0){
-                    if(bcrypt.compareSync(password,results[0].password) || password==results[0].password){
-                        var sql ='SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
-                        var insert =[username]
-                        var result =  mysql.format(sql,insert);
-        
-                        connection.query(result, function (error, result, fields) {
-                            if (error) {
-                                console.log("Bad Request",error);
-                                return res.status(404).send({ message: 'Bad Request' });
+    }
+    console.log("user" + username, "password " + password);
+    connection.query('SELECT * FROM users WHERE email = ?', username, function (error, results, fields) {
+        if (error) {
+            return res.status(404).send({ message: 'Not found, email provide is not in available' });
+        } else {
+            if (results.length > 0) {
+                if (bcrypt.compareSync(password, results[0].password) || password == results[0].password) {
+                    var sql = 'SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
+                    var insert = [username]
+                    var result = mysql.format(sql, insert);
+
+                    connection.query(result, function (error, result, fields) {
+                        if (error) {
+                            console.log("Bad Request", error);
+                            return res.status(404).send({ message: 'Not Found, cannot get any user for this email' });
                         }
-                        else{
+                        else {
                             res.send(result[0]);
                         }
                     });
                 }
-                else{
-                    return res.status(401).send({ message: 'Unauthorized' });
+                else {
+                    return res.status(401).send({ message: 'Unauthorized, password does not match the current user' });
                 }
             }
-            else{
-                return res.status(404).send({ message: 'Bad Request' });
-                }
+            else {
+                return res.status(404).send({ message: 'Not found, user not found for this email' });
             }
-        });
+        }
+    });
 };
---
+++ webapp/controllers/app.imageController.js
@@ -0,0 +1,246 @@
+var bcrypt = require('bcrypt');
+var mysql = require('mysql');
+var connection = require('../models/app.model');
+const uuidv1 = require('uuid/v1');
+const aws = require('aws-sdk');
+var fs = require('fs');
+require('dotenv').config();
+
+aws.config.update({
+  secretAccessKey: process.env.secretAccessKey,
+  accessKeyId:process.env.accessKeyId,
+  region: process.env.region
+});
+var s3 = new aws.S3();
+
+
+exports.addRecipeImage = function (req, res, next) {
+  var token = req.headers['authorization'];
+  if (!token) return res.status(400).send({ message: 'Bad Request,' });
+  var recipeid = req.params['recipeId'];
+  var tmp = token.split(' ');
+  var buf = new Buffer(tmp[1], 'base64');
+  var plain_auth = buf.toString();
+  var creds = plain_auth.split(':');
+  var username = creds[0];
+  var password = creds[1];
+  if (username == null || password == null) return res.status(400).send({ message: 'Bad Request, Password and Username cannot be null' });
+  if(req.file==null) return res.status(400).send({ message: 'Bad Request, Image in form data cannot be null' });
+  if(req.file.mimetype==="image/png" ||req.file.mimetype==="image/jpg" || req.file.mimetype==="image/jpeg" )
+  { 
+  connection.query('SELECT * FROM users WHERE email = ?', username, function (error, results, fields) {
+    var userid = "";
+    if (error) {
+      return res.status(400).send({ message: 'Bad Request' });
+    }
+    if (results.length < 0 || typeof results[0] === 'undefined') {
+      return res.status(404).send({ message: 'Not Found, user not found' });
+
+    }
+    if (!bcrypt.compareSync(password, results[0].password)) {
+
+      return res.status(401).send({ message: 'Unauthorized User,Password does not match for the current user' });
+
+    }
+   
+
+    userid = results[0].id;
+    var ins = [recipeid, userid]
+    var resultsSelectqlquerry = mysql.format('SELECT * FROM recipe where id= ? AND author_id=?', ins);
+    connection.query(resultsSelectqlquerry, function (error, results, fields) {
+      if (error) {
+        return res.status(404).send({ message: 'Recipe not found' });
+      }
+      if (results.length < 0) {
+
+        return res.status(404).send({ message: 'Not Found, Recipe not found for this user' });
+      }
+      else {
+        console.log(req.file);
+        console.log("----")
+        var image_name = req.file.originalname;
+        var selectSql = "SELECT count(imageName) AS Count  FROM Images WHERE imageName = ? and recipeTable_idrecipe=?";
+        var insert = [image_name,recipeid];
+        var result = mysql.format(selectSql, insert);
+        console.log(result)
+        connection.query(result, function (error, result, fields) {
+          if (error) {
+            console.log(error);
+            return res.status(404).send({ message: 'Npt' });
+          } 
+          var count = result[0].Count;
+          console.log("------------" + count)
+          if (count >= 1) {
+              return res.status(400).send({ message: 'Bad Request, Image allready exist , please delete the image and post it' });
+          }
+          var imageid = uuidv1();
+
+
+        var fileStream = fs.createReadStream(req.file.path);
+        fileStream.on('error', function (err) {
+          console.log('File Error', err);
+        });
+        console.log(process.env.bucket)
+        var uploadParams = { Bucket: process.env.bucket, Key: imageid, Body: '' };
+        uploadParams.Body = fileStream;
+        s3.upload(uploadParams, function (err, data1) {
+          if (err) {
+            console.log(err);
+            return res.status(400).send({ message: 'Bad Request, Please Add image correctly' });
+          } if (data1) {
+            var param1 = { Bucket: process.env.bucket, Key: imageid};
+            s3.getObject(param1, function(err, data) {
+              if (err) console.log(err, err.stack); // an error occurred
+              else    {
+                 console.log(data); 
+                console.log(data1);
+                }        // successful response
+            
+            // res.send("uploded")
+            var image = {
+              id: imageid,
+              imageName: image_name,
+              url: data1.Location,
+              recipeTable_idrecipe: recipeid,
+              AcceptRanges: data.AcceptRanges,
+              LastModified: data.LastModified,
+              ContentLength: data.ContentLength,
+              ETag: data.ETag,
+              ContentType: data.ContentLength,
+            }
+            connection.query('INSERT INTO Images SET ?', image, function (error, results, fields) {
+              if (error) {
+                console.log("Bad Request", error);
+                res.status(400).send({
+                  "failed": "Bad Request, Cannot enter recipe"
+                })
+              } else {
+                res.status(201).send({ 
+                  
+                  "id": imageid,
+                  "url":data1.Location
+                 });
+              }
+            });
+          });
+          }
+        });
+        });
+      }
+    });
+  });
+
+}else{
+  return res.status(400).send({ message: 'Bad Request, Please Add image in correct format' });
+}
+}
+
+exports.getRecipeImage = function (req, res) {
+console.log("hi")
+  var imageid = req.params['imageId'];
+  var recipeTable_idrecipe = req.params['recipeId'];
+
+  var ins = [ imageid, recipeTable_idrecipe]
+
+  var resultsSelectqlquerry = mysql.format('SELECT * FROM Images where id= ? and recipeTable_idrecipe=? ', ins);
+  connection.query(resultsSelectqlquerry, function (error, results, fields) {
+    if (error) {
+      return res.status(400).send({ message: 'Bad Request' });
+    }
+    if (results.length < 0 || typeof results[0] === 'undefined') {
+      return res.status(404).send({ message: 'Not Found, user not found' });
+
+    }
+    else {
+      var param1 = { Bucket: process.env.bucket, Key: imageid};
+            s3.getObject(param1, function(err, data) {
+              if (err)  return res.status(404).send({ message: 'Not Found, user not found' });
+              // an error occurred
+            });
+      res.status(201).send({ 
+        "id": imageid,
+        "url":results[0].url
+       });
+    }
+  });
+};
+
+exports.deleteRecipeImage = function (req, res) {
+  var token = req.headers['authorization'];
+  if (!token) return res.status(400).send({ message: 'Bad Request,Please provide Authorization' });
+  var recipeid = req.params['recipeId'];
+  var imageId = req.params['imageId'];
+  var tmp = token.split(' ');
+  var buf = new Buffer(tmp[1], 'base64');
+  var plain_auth = buf.toString();
+  var creds = plain_auth.split(':');
+  var username = creds[0];
+  var password = creds[1];
+console.log(username)
+  if (username == null || password == null) return res.status(400).send({ message: 'Bad Request, Password and Username cannot be null' });
+  connection.query('SELECT * FROM users WHERE email = ?', username, function (error, results, fields) {
+    var userid = "";
+    if (error) {
+      return res.status(400).send({ message: 'Bad Request' });
+    }
+    if (results.length < 0 || typeof results[0] === 'undefined') {
+      return res.status(404).send({ message: 'Not Found, user not found' });
+
+    }
+    if (!bcrypt.compareSync(password, results[0].password)) {
+
+      return res.status(401).send({ message: 'Unauthorized User,Password does not match' });
+
+    }
+    userid = results[0].id;
+    var ins = [recipeid, userid]
+    var resultsSelectqlquerry = mysql.format('SELECT * FROM recipe where id= ? AND author_id=?', ins);
+    connection.query(resultsSelectqlquerry, function (error, results, fields) {
+      if (error) {
+        return res.status(404).send({ message: 'Recipe  Not Found' });
+      }
+      if (results.length < 0) {
+
+        return res.status(404).send({ message: 'Not Found, Recipe not found for this user' });
+      }
+      else {
+        console.log("----")
+        var selectSql = "SELECT count(imageName) AS Count,url,imageName,id  FROM Images WHERE id = ?";
+        var insert = [imageId];
+        var result = mysql.format(selectSql, insert);
+        connection.query(result, function (error, result, fields) {
+          var count = result[0].Count;
+          console.log("------------" + count)
+          if (count < 1) {
+              return res.status(404).send({ message: 'Not Found, Image doesnot exist' });
+          }
+          
+     
+     
+          var deleteParams = {Bucket: process.env.bucket,  Key: result[0].id};
+          s3.deleteObject(deleteParams, function (err, data) {
+            if (err) {
+              console.log(err, err.stack);
+              return res.status(400).send({ message: 'Bad Request, Please Add image correctly' });
+            } else{
+                         
+              console.log("Delete Success" );
+              connection.query('delete from Images where id= ?', imageId, function (error, results, fields) {
+                if (error) {
+                  console.log("Bad Request", error);
+                  res.status(400).send({
+                    "failed": "Bad Request, Cannot Delete recipe"
+                  })
+                } else {
+                  console.log(data);
+                  res.status(204).send({ message: 'No Content' });
+                }
+              });
+            }
+          })  
+        })
+      }
+    });
+  });
+};
+
