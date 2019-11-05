cd6c6bd4d9042575f337810b372b1ede265a6206
achirashah <shah.ac@husky.neu.edu>
2019-11-05 17:37:27 -0500

    cw

--- infrastructure/aws/codedeploy/applicationstart.sh
+++ infrastructure/aws/codedeploy/applicationstart.sh
@@ -7,4 +7,4 @@ nohup node server.js >> app.log 2>&1 &
 
 echo "starting application on 3001"
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/webapp/cloudwatch-agent-config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/webapp/cloudwatch-config.json -s
