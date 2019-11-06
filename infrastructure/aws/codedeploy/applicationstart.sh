f87fa7586ce585805e836ae5eb18f25a83a2407d
arjunagrawal <agrawal.arj@northeastern.edu>
2019-11-05 23:48:58 -0500

    updated cloudwatch

--- infrastructure/aws/codedeploy/applicationstart.sh
+++ infrastructure/aws/codedeploy/applicationstart.sh
@@ -3,6 +3,9 @@
 cd /home/centos/webapp/
 npm install
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/webapp/cloudwatch-config.json -s
+sudo rm -r node_modules
+sudo rm -r logs
+sudo rm app.log
 
 nohup node server.js >> app.log 2>&1 &
 
