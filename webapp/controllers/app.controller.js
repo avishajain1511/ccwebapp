ad97ea114f4a376b0166d28fb35947a33b81a66d
arjunagrawal <agrawal.arj@northeastern.edu>
2019-11-06 10:25:51 -0500

    updated cloudwatch metrics for database timmer

--- webapp/controllers/app.controller.js
+++ webapp/controllers/app.controller.js
@@ -13,7 +13,6 @@ var getCounter=0;
 
 exports.register = function (req, res) {
        logger.info("Register User");
-       client.count("Rgister Api call", 1);
     client.increment('Register');
     var start = new Date();
 
@@ -33,7 +32,9 @@ client.count("count register api", registerCounter);
 
     if (!validator.validate(req.body.email)) { return res.status(400).send({ message: 'Bad Request, invalid Email' }) };
 
+
     if (!schema.validate(req.body.password)) { return res.status(400).send({ message: 'Bad Request, invalid Password' }) };
+    
 
     var salt = bcrypt.genSaltSync(10);
     var hash = bcrypt.hashSync(req.body.password, salt);
@@ -59,9 +60,10 @@ client.count("count register api", registerCounter);
             return res.status(400).send({ message: 'Bad Request, Invalid email' });
         }
         connection.query('INSERT INTO users SET ?', users, function (error, results, fields) {
-            setTimeout(function () {
-                client.timing('insert user timming', start);
-            }, 100 * Math.random());
+            var end = new Date();
+            var dif=end-start;
+            console.log(dif);
+            client.count("time to add user", dif);
             if (error) {
                 console.log("Bad Request, cannot insert user", error);
                 res.send({
@@ -69,6 +71,8 @@ client.count("count register api", registerCounter);
                     "failed": "Bad Request"
                 })
             }
+         
+
             var sql = 'SELECT id , firstname, lastname, email, created, modified  FROM users WHERE email = ?';
             var insert = [users.email]
             var result = mysql.format(sql, insert);
