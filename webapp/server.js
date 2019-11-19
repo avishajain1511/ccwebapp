ecb1293502c49330c1925060b153239eb8b1b547
arjunagrawal <agrawal.arj@northeastern.edu>
2019-11-19 11:30:46 -0500

    removed  http

--- webapp/server.js
+++ webapp/server.js
@@ -18,11 +18,6 @@ server.listen(port,()=>{
 console.log('Server is up and running on port number' + port);
 
 });
-server.use(function(req, res, next) {
-  if ((req.get('X-Forwarded-Proto') !== 'https')) {
-    res.redirect('https://' + req.get('Host') + req.url);
-  } else
-    next();
-});
+
 module.exports = server;
 
